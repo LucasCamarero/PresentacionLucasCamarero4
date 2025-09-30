31914f6aa48149b01ce84f5401da9572b574c0fc
LucasCamarero <l.camareroperez@ikasle.eus>
2025-09-30 17:31:41 +0200

    feat: clonacion portatil txurdinaga

--- app/src/main/java/com/lucascamarero/presentacionlucascamarero2/MainActivity.kt
+++ app/src/main/java/com/lucascamarero/presentacionlucascamarero2/MainActivity.kt
@@ -47,7 +47,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lucascamarero.presentacionlucascamarero2.ui.theme.PresentacionLucasCamarero2Theme
 import androidx.core.net.toUri
-
+//
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
