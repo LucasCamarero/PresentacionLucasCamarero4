9c7db25ca31974308e517f82def9bfe64fa15002
LucasCamarero <l.camareroperez@ikasle.eus>
2025-10-02 21:03:59 +0200

    feat: novedad2, uso de un checkbox en VentanaSettings.kt

--- app/src/main/java/com/lucascamarero/presentacionlucascamarero2/VentanaSettings.kt
+++ app/src/main/java/com/lucascamarero/presentacionlucascamarero2/VentanaSettings.kt
@@ -2,9 +2,15 @@ package com.lucascamarero.presentacionlucascamarero2
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -13,18 +19,26 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 /**
- * Pantalla de ajustes sin desarrollar.
+ * Cambia un texto en base a si el checkbox está o no activado
  *
  * @param navController Controlador de navegación para gestionar rutas (no utilizado).
  */
 @Composable
 fun VentanaSettings(navController: NavController) {
-    var presses by remember { mutableIntStateOf(0) }
-    val context = LocalContext.current
-    Column(
-        modifier = Modifier,
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
+    var checked by remember { mutableStateOf(false) }
 
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
+
+            Checkbox(
+                checked = checked,
+                onCheckedChange = { checked = it }
+            )
+
+            Text(text = if (checked) "Notificaciones activadas" else "Notificaciones desactivadas")
+        }
     }
 }
